7ba6c7261bc3a11b0f600b3631ee00ad9b80413c
borty12 <Jeffrey.Bortman@tufts.edu>
2016-10-11 23:47:08 -0400

    Second commit in the development stage

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -1,12 +1,25 @@
-<script>
-	var map;
-	function initMap(){
-		map= new google.maps.Map(document.getElementById('map'),{
-			center: {lat: -34.397, lng: 150.644},
-			zoom: 8 
-		});
-</script>
+var map;
+var parsedData;
 
- <script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=https://rocky-taiga-26352.herokuapp.com/redline.json&callback=initMap">
-    </script>
+function initMap(){
+	request = new XMLHttpRequest();
+	request.open("get", "https://rocky-taiga-26352.herokuapp.com/redline.json", true);
+	request.onreadystatechange = funex;
+	request.send();
+}
+
+function funex(){
+	console.log("called funex" + request.readyState);
+	if (request.readyState == 4 && request.status == 200){
+		theData=request.responseText;
+		funex = JSON.parse(theData);
+		newHTML = "";
+		section = document.getElementById("map");
+
+	}
+}
+
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 12
+});
